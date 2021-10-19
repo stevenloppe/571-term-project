9ead36c0900a34c768d4c1c2bfc88ee2aa11d54f
Jan-Jelinek <jankojelinek@gmail.com>
2021-10-19 13:21:30 -0600

    Added bootstrap + index

--- webApp/views.py
+++ webApp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates"),'
+)
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request,"index.html")
+
+#class IndexView(generic.DetailView):
+#    template_name = 'webApp/index.html'
