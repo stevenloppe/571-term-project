a6677137e23ff71b4efecbf81dca411eee6008c1
Jan-Jelinek <jankojelinek@gmail.com>
2021-11-02 11:07:57 -0600

    Changed EmojiTranslation to function

--- main/EmojiTranslation.py
+++ main/EmojiTranslation.py
@@ -1,23 +1,24 @@
 import csv
 
-inputList = ["Eyes", "Rocket", "Gem stone", "Raising hands"]
-sentiments = []
-output = 0.5
+def EmojiTranslation(inputList):
+    #inputList = ["Eyes", "Rocket", "Gem stone", "Raising hands"]
+    sentiments = []
+    output = 0.5
 
-with open("Emoji_Sentiment_Data_v1.0.csv", 'r', encoding="utf8") as csvfile:
-    csvreader = csv.reader(csvfile)
-    for row in csvreader:
-        name = row[7]
-        for item in inputList:
-            if (name == item.upper()):
-                negative = int(row[4])
-                neutral = int(row[5])
-                positive = int(row[6])
-                # Formula: ( Positive + Neutral/2 ) / Total
-                sentiment = (positive+neutral/2)/(negative+neutral+positive)
-                sentiments.append(sentiment)
+    with open("Emoji_Sentiment_Data_v1.0.csv", 'r', encoding="utf8") as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            name = row[7]
+            for item in inputList:
+                if (name == item.upper()):
+                    negative = int(row[4])
+                    neutral = int(row[5])
+                    positive = int(row[6])
+                    # Formula: ( Positive + Neutral/2 ) / Total
+                    sentiment = (positive+neutral/2)/(negative+neutral+positive)
+                    sentiments.append(sentiment)
 
-if (len(sentiments) > 0):
-    output = sum(sentiments) / len(sentiments)
+    if (len(sentiments) > 0):
+        output = sum(sentiments) / len(sentiments)
 
-print("\nOutput: ",output)
+    return output
